fbfbf8a856ae85198cca118f0aa9ec8c3a6c3f9d
ArestrepoBlankfactor <alejandro.restrepo@blankfactor.com>
2022-02-10 15:37:02 -0500

    add first commands to shopping page

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,32 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import ProductViewPage from "../pages/ProductViewPage";
+
+const productViewPage = new ProductViewPage();
+
+Cypress.Commands.add("gotoTheShoppingPage", () => {
+  cy.log("visit shopping automation page");
+  cy.visit(Cypress.env("URL"));
+  cy.get("#header_logo").should("be.visible");
+});
+
+Cypress.Commands.add("searchProduct", (productName) => {
+  cy.log("search a product to buy");
+  cy.get("#search_query_top")
+    .should("have.attr", "placeholder", "Search")
+    .type(productName)
+    .type("{enter}");
+});
+
+Cypress.Commands.add("switchToIframeProductView", () => {
+  cy.log("change to the Iframe context");
+  cy.waitLoader();
+  return cy
+    .get(productViewPage.elements.iframeViewProduct())
+    .its("0.contentDocument.body")
+    .should("be.visible")
+    .then(cy.wrap);
+});
+
+Cypress.Commands.add("waitLoader", () => {
+  cy.log('waiting the loader disappear')
+  cy.get("#fancybox-loading > div", { timeout: 30000 }).should("not.exist");
+});
